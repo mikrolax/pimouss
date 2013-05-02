afca8cf925f3ab4f25aa0cf8036ff61dadf01819
mikrolax <seb@mikrolax.me>
2013-05-02 23:55:34 +0200

    improved packaging. Temporary script name is igloo.

--- setup.py
+++ setup.py
@@ -16,15 +16,18 @@ setup(name='pimouss',
       author='sebastien stang',
       author_email='seb@mikrolax.me',
       url='',
-      package_dir = {'': 'pimouss'},
-      packages=['external'],
+      #package_dir = {'': 'pimouss'},
+      packages=['pimouss','pimouss.external','pimouss.desktop'],
       py_modules=['pimouss'], 
-      scripts=['pimouss/pimouss.py','pimouss/pimouss-gui.py'],
+      scripts=['igloo.py'], #pimouss
+
+      package_data={'desktop': ['static/*.png'],
+                  },
 
       #for py2exe 
       options ={'py2exe': {'excludes':['mail','unittest','jinja2','pygments'],'bundle_files': 1,'dist_dir':'win32-static/'}}, 
       zipfile = None,
-      console=[{'script':'pimouss/pimouss.py',}],
+      #console=[{'script':'pimouss/pimouss.py',}],
       windows=[{'script':'pimouss/pimouss-gui.py'}]
      )
 
